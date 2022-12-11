b4c972a4944e6bae49101c30294628477b9efa8d
Sanju Josh <sanjujosh@users.noreply.github.com>
2022-12-11 22:30:36 +0000

    added API endpoint with current time and returning json format

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -17,6 +18,7 @@ var (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", whoamiHandler)
+	mux.HandleFunc("/api", apiHandler)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -58,6 +60,52 @@ func whoamiHandler(w http.ResponseWriter, req *http.Request) {
 
 	_, _ = fmt.Fprintln(w, "RemoteAddr:", req.RemoteAddr)
 	if err := req.Write(w); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func apiHandler(w http.ResponseWriter, req *http.Request) {
+	hostname, _ := os.Hostname()
+
+	data := struct {
+		Hostname string      `json:"hostname,omitempty"`
+		IP       []string    `json:"ip,omitempty"`
+		Headers  http.Header `json:"headers,omitempty"`
+		URL      string      `json:"url,omitempty"`
+		Host     string      `json:"host,omitempty"`
+		Method   string      `json:"method,omitempty"`
+		Time     time.Time   `json:"time,omitempty"`
+	}{
+		Hostname: hostname,
+		IP:       []string{},
+		Headers:  req.Header,
+		URL:      req.URL.RequestURI(),
+		Host:     req.Host,
+		Method:   req.Method,
+		Time:     time.Now(),
+	}
+
+	ifaces, _ := net.Interfaces()
+	for _, i := range ifaces {
+		addrs, _ := i.Addrs()
+		// handle err
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip != nil {
+				data.IP = append(data.IP, ip.String())
+			}
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
