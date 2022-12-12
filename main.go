346082abf4fd0d5355d0028ccc667327a4eb2471
Sanju Josh <sanjujosh@users.noreply.github.com>
2022-12-12 18:40:05 +0000

    EC2 metadata collection

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -11,11 +13,28 @@ import (
 	"time"
 )
 
+// EC2 Meta data end point
+const (
+	baseUrl = "http://169.254.169.254/latest/meta-data"
+)
+
 var (
-	port = "80"
+	port   = ""
+	region = ""
+	az     = ""
 )
 
 func main() {
+	// Parse command line arguments
+	flag.StringVar(&port, "port", getEnv("PORT_NUMBER", "8080"), "port number to listen")
+	flag.Parse()
+
+	// Fetch EC2 meta data
+	if err := extractEc2Meta(); err == nil {
+		log.Printf("Region: %v", region)
+		log.Printf("AZ: %v", az)
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", whoamiHandler)
 	mux.HandleFunc("/api", apiHandler)
@@ -27,6 +46,20 @@ func main() {
 	log.Printf("Server starting on %s", server.Addr)
 	log.Fatal(server.ListenAndServe())
 
+}
+
+// Extract EC2 Meta data
+func extractEc2Meta() error {
+	var err error
+	// Region
+	urlRegion := fmt.Sprintf("%v/placement/region", baseUrl)
+	region, err = getMeta(urlRegion)
+
+	// Availability Zone
+	urlAz := fmt.Sprintf("%v/placement/availability-zone", baseUrl)
+	az, err = getMeta(urlAz)
+
+	return err
 }
 
 func whoamiHandler(w http.ResponseWriter, req *http.Request) {
@@ -70,6 +103,8 @@ func apiHandler(w http.ResponseWriter, req *http.Request) {
 
 	data := struct {
 		Hostname string      `json:"hostname,omitempty"`
+		Region   string      `json:"region,omitempty"`
+		AZ       string      `json:"az,omitempty"`
 		IP       []string    `json:"ip,omitempty"`
 		Headers  http.Header `json:"headers,omitempty"`
 		URL      string      `json:"url,omitempty"`
@@ -78,6 +113,8 @@ func apiHandler(w http.ResponseWriter, req *http.Request) {
 		Time     time.Time   `json:"time,omitempty"`
 	}{
 		Hostname: hostname,
+		Region:   region,
+		AZ:       az,
 		IP:       []string{},
 		Headers:  req.Header,
 		URL:      req.URL.RequestURI(),
@@ -109,4 +146,28 @@ func apiHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func getMeta(url string) (string, error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(bodyBytes), nil
+}
+
+func getEnv(key, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
 }
